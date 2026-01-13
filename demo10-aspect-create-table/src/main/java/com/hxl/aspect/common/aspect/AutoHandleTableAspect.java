0c4a68198bc3b66351d2fb12f9b1dadb2df6405d
hengxiaoliang <hengxl@vvmeeting.com>
2026-01-13 18:55:32 +0800

    [2026/1/13]AOP设计--抽离与Calendar相关的切面

--- demo10-aspect-create-table/src/main/java/com/hxl/aspect/common/aspect/AutoHandleTableAspect.java
+++ demo10-aspect-create-table/src/main/java/com/hxl/aspect/common/aspect/AutoHandleTableAspect.java
@@ -1,7 +1,6 @@
 package com.hxl.aspect.common.aspect;
 
 import com.google.common.base.Throwables;
-import com.hxl.aspect.common.annotation.EnableFillCalendar;
 import com.hxl.aspect.common.model.ITableNameHolder;
 import com.hxl.aspect.database.IDatabaseService;
 import com.hxl.aspect.mapper.CalendarMapper;
@@ -9,19 +8,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
-import java.lang.reflect.Method;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -40,55 +35,25 @@ public class AutoHandleTableAspect {
     @Autowired
     private CalendarMapper calendarMapper;
 
-    /**
-     * key: 要执行的方法 Method
-     * value: 是否标注了 @EnableFillCalendar 注解
-     */
-    private static final Map<Method, Boolean> METHOD_ANNOTATION_CACHE = new HashMap<>(64);
-
-    private static final String DEDUCTION_TABLE_TEMPLATE = "t_deduction";
-    private static final String MAX_ONLINE_NUM_TABLE_TEMPLATE = "t_max_online_num";
-
-
     @Around("execution(* com.hxl.aspect.mapper.DeductionMapper.*(..))")
     public Object handleCalendar(ProceedingJoinPoint joinPoint)
             throws Throwable {
-        return autoHandleTables(joinPoint, DEDUCTION_TABLE_TEMPLATE);
+        return handleMultiTables(joinPoint, true);
     }
 
     /**
-     * 自动处理表数据异常
-     *
-     * @param joinPoint     目标方法
-     * @param tableTemplate 表模板
+     * 自动创建表 + 填充日历表
      */
-    private Object autoHandleTables(ProceedingJoinPoint joinPoint, String tableTemplate)
+    private Object handleMultiTables(ProceedingJoinPoint joinPoint, boolean needFillCalendar)
             throws Throwable {
         String tableName = null;
         try {
-            Method targetMethod = ((MethodSignature) joinPoint.getSignature()).getMethod();
-            // 缓存命中 => 已处理过 targetMethod
-            if (METHOD_ANNOTATION_CACHE.containsKey(targetMethod)) {
-                // 标注了 @EnableFillCalendar 注解
-                if (METHOD_ANNOTATION_CACHE.get(targetMethod)) {
-                    tableName = getTableNameFromArgs(joinPoint.getArgs());
-                    fillCalendar(tableName);
-                    // 执行目标方法
-                    return joinPoint.proceed();
-                }
-                // 未标注 @EnableFillCalendar 注解, 直接执行方法返回最新结果
-                return joinPoint.proceed();
-            }
-
-            // Map缓存不存在记录 => 未处理过目标方法，先执行一次判断注解
-            EnableFillCalendar annotation = AnnotationUtils.findAnnotation(targetMethod, EnableFillCalendar.class);
-            if (Objects.nonNull(annotation)) {
-                METHOD_ANNOTATION_CACHE.put(targetMethod, true);
+            // 如果需要填充日历表，先获取表名并填充
+            if (needFillCalendar) {
                 tableName = getTableNameFromArgs(joinPoint.getArgs());
                 fillCalendar(tableName);
-                return joinPoint.proceed();
             }
-            METHOD_ANNOTATION_CACHE.put(targetMethod, false);
+            // 执行目标方法
             return joinPoint.proceed();
         } catch (Throwable ex) {
             Throwable root = Throwables.getRootCause(ex);
@@ -97,6 +62,8 @@ public class AutoHandleTableAspect {
                 if (Objects.isNull(tableName)) {
                     tableName = getTableNameFromArgs(joinPoint.getArgs());
                 }
+                // 从表名中获取模板表的名字
+                String tableTemplate = getTableTemplate(tableName);
                 // 创建表
                 databaseService.createTable(tableName, tableTemplate);
                 return joinPoint.proceed();
@@ -107,7 +74,7 @@ public class AutoHandleTableAspect {
     }
 
     /**
-     * 判断是否需要填充 calendar 表数据
+     * 填充 calendar 表数据
      */
     private void fillCalendar(String tableName) {
         String[] tableNameSplit = tableName.split("_");
@@ -131,6 +98,14 @@ public class AutoHandleTableAspect {
         }
     }
 
+    /**
+     * 从表名中获取模板表的名字, 格式: tableName <=> tableTemplate_year_month
+     */
+    private static @NonNull String getTableTemplate(String tableName) {
+        int firstLastIndex = tableName.lastIndexOf("_");
+        int secondLastUnderlineIndex = tableName.lastIndexOf("_", firstLastIndex - 1);
+        return tableName.substring(0, secondLastUnderlineIndex);
+    }
 
     /**
      * 规约: 方法的某一个参数必须实现 ITableNameHolder 接口, 才能获取表名
